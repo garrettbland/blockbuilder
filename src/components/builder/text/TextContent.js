57d6b9fe2e27fd79fa99fee0b485c4ce6b075769
garrettbland <gmorganbland@gmail.com>
2020-11-20 15:00:08 -0600

    cleaned up

--- src/components/builder/text/TextContent.js
+++ src/components/builder/text/TextContent.js
@@ -1,30 +1,10 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { UPDATE_EDITING } from '@/redux/constants'
 import dynamic from 'next/dynamic'
 const TrixEditor = dynamic(() => import('@/components/builder/TrixEditor'), {
     ssr: false,
 })
-// const TrixEditor = dynamic(() => import('react-trix-rte').then((mod) => mod.ReactTrixRTEInput), {
-//     ssr: false,
-// })
-// const TrixEditor = dynamic(() => import('@/components/builder/TrixEditor'), {
-//     ssr: false,
-// })
-// import { ReactTrixRTEInput } from 'react-trix-rte'
-//import dynamic from 'next/dynamic'
-
-/**
- * Working on this crazy dynamic imports issue...
- */
-
-// const Trix = dynamic(() => import('react-trix-rte').then((module) => module.ReactTrixRTEInput), {
-//     ssr: false,
-// })
-
-// const Trix = dynamic(() => import('react-trix-rte').then((module) => module.ReactTrixRTEInput), {
-//     ssr: false,
-// })
 
 const TextContent = () => {
     const currentlyEditing = useSelector((state) => state.currentlyEditing)
