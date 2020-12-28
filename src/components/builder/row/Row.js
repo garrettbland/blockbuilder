65b821be6ac9e2c33e00fbe01fd0c51d7ac5b856
garrettbland <gmorganbland@gmail.com>
2020-12-27 22:29:25 -0600

    udpating styling of row hover

--- src/components/builder/row/Row.js
+++ src/components/builder/row/Row.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState, useRef } from 'react'
 import { returnFound } from 'find-and'
 import { useDispatch, useSelector } from 'react-redux'
 import { SET_EDITING, APPEND_ROW, DUPLICATE_BLOCK, SET_MODAL_VISIBILITY } from '@/redux/constants'
+import { Settings, Copy } from 'react-feather'
 
 const Row = ({ block, children }) => {
     const [showTool, setShowTool] = useState(false)
@@ -47,39 +48,39 @@ const Row = ({ block, children }) => {
         })
     }
 
+    const handleSettingsClick = () => {
+        dispatch({
+            type: SET_MODAL_VISIBILITY,
+            payload: true,
+        })
+        dispatch({
+            type: SET_EDITING,
+            payload: returnFound(blocks, { id: block.id }),
+        })
+    }
+
     return (
         <div data-type="row" ref={rowRef} className={[...block.classList].join(' ')}>
-            <div className={`absolute top-0 left-0 flex flex-row ${showTool ? 'block' : 'hidden'}`}>
-                <button
-                    onClick={() => {
-                        dispatch({
-                            type: SET_MODAL_VISIBILITY,
-                            payload: true,
-                        })
-                        dispatch({
-                            type: SET_EDITING,
-                            payload: returnFound(blocks, { id: block.id }),
-                        })
-                    }}
-                    className={`bg-red-500`}
-                >
-                    Row Tools
-                </button>
-                <button
-                    onClick={() => DuplicateBlock()}
-                    className="bg-pink-500 text-white cursor-pointer"
-                >
-                    Duplicate
-                </button>
+            <div
+                className={`absolute top-0 left-0 flex flex-row w-full h-full ${
+                    showTool
+                        ? 'block ring-4 ring-orange-600 ring-opacity-70 rounded-lg overflow-hidden'
+                        : 'hidden'
+                }`}
+            >
+                <div className="absolute top-0 left-0 flex flex-row items-center bg-orange-600 bg-opacity-70 rounded-br-lg">
+                    <Settings
+                        onClick={() => handleSettingsClick()}
+                        strokeWidth={1.3}
+                        className="w-10 h-10 text-orange-900 hover:text-black p-2 cursor-pointer"
+                    />
+                    <Copy
+                        strokeWidth={1.3}
+                        className="w-10 h-10 text-orange-900 hover:text-black p-2 cursor-pointer"
+                        onClick={() => DuplicateBlock()}
+                    />
+                </div>
             </div>
-            {showTool && (
-                <>
-                    <div className="absolute left-0 top-0 bg-orange-500 w-1 h-full z-20"></div>
-                    <div className="absolute right-0 top-0 bg-orange-500 w-1 h-full z-20"></div>
-                    <div className="absolute left-0 top-0 bg-orange-500 w-full h-1 z-20"></div>
-                    <div className="absolute left-0 bottom-0 bg-orange-500 w-full h-1 z-20"></div>
-                </>
-            )}
             {children}
             <div
                 className={`absolute bottom-0 left-0 w-full h-0 bg-orange-500 flex items-center justify-center z-30 ${
