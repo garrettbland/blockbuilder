ff03067c576cc1d913be775a101c6c4ab68182d8
garrettbland <gmorganbland@gmail.com>
2021-01-08 00:04:37 -0600

    updating email form, SSR page

--- src/pages/pages/[id].js
+++ src/pages/pages/[id].js
@@ -1,23 +1,57 @@
-const Page = () => {
+var admin = require('firebase-admin')
+import Head from 'next/head'
+
+const Page = ({ blocks, id, error = false }) => {
+    if (error) {
+        return <div>Something went wrong on our end</div>
+    }
+
     return (
         <div>
-            <div>Fetch firebase page</div>
+            <Head>
+                <title>Block Builder</title>
+            </Head>
+            <div>
+                <div>Fetch firebase page for {id}</div>
+                <div>{JSON.stringify(blocks, null, 4)}</div>
+            </div>
         </div>
     )
 }
 
 export const getServerSideProps = async (context) => {
-    // Fetch data from external API
-    // const res = await fetch(`https://.../data`)
-    // const data = await res.json()
+    try {
+        var serviceAccount = require('@/src/firebase-admin.json')
+        if (!admin.apps.length) {
+            admin.initializeApp({
+                credential: admin.credential.cert(serviceAccount),
+            })
+        }
 
-    // Pass data to the page via props
-    // return { props: { data } }
+        /**
+         * Get the id from the URL /pages/[id]
+         */
+        const document = await admin.firestore().collection('beta').doc(context.params.id).get()
 
-    return {
-        props: {
-            id: context.params.id,
-        },
+        if (!document.exists) {
+            return {
+                notFound: true,
+            }
+        } else {
+            return {
+                props: {
+                    id: context.params.id,
+                    blocks: document.data().blocks,
+                },
+            }
+        }
+    } catch (err) {
+        console.log(err)
+        return {
+            props: {
+                error: true,
+            },
+        }
     }
 }
 
