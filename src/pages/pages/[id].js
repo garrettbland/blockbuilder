0d5032b16b7505e04f3b34eb021146d19da0071d
garrettbland <gmorganbland@gmail.com>
2021-01-09 13:56:13 -0600

    removed ssr, setup client rendering

--- src/pages/pages/[id].js
+++ src/pages/pages/[id].js
@@ -1,11 +1,57 @@
-var admin = require('firebase-admin')
+// var admin = require('firebase-admin')
+import { useEffect, useState } from 'react'
 import Head from 'next/head'
+import firebase from '@/src/firebase'
+import { useRouter } from 'next/router'
 import { SerializeToHtml } from '@/utils/serialize'
 
-const Page = ({ blocks, id, error = false }) => {
-    console.log(blocks)
-    if (error) {
-        return <div>Something went wrong on our end</div>
+const Page = () => {
+    const [isLoading, setIsLoading] = useState(true)
+    const [hasError, setHasError] = useState(false)
+    const [blocks, setBlocks] = useState([])
+    const router = useRouter()
+    const { id } = router.query
+
+    useEffect(() => {
+        /**
+         * Load blocks on page load
+         */
+        if (id) {
+            fetchBlocks().then(({ blocks }) => {
+                setBlocks(blocks)
+                setIsLoading(false)
+            })
+        }
+    }, [id])
+
+    const fetchBlocks = async () => {
+        try {
+            const document = await firebase.firestore().collection('beta').doc(id).get()
+            if (!document.exists) {
+                console.log('doc doesnt exist')
+                return {
+                    error: true,
+                }
+            } else {
+                console.log(blocks)
+                return {
+                    blocks: document.data().blocks,
+                }
+            }
+        } catch (err) {
+            console.log(err)
+            return {
+                error: true,
+            }
+        }
+    }
+
+    if (isLoading) {
+        return (
+            <div className="my-12">
+                <div className="max-w-xl mx-auto text-center text-lg text-gray-800">Loading...</div>
+            </div>
+        )
     }
 
     return (
@@ -13,51 +59,53 @@ const Page = ({ blocks, id, error = false }) => {
             <Head>
                 <title>Block Builder</title>
             </Head>
-            <div>
-                {/* <div>Fetch firebase page for {id}</div>
+            {!isLoading && (
+                <div>
+                    {/* <div>Fetch firebase page for {id}</div>
                 <pre>
                     <code>{JSON.stringify(blocks, null, 4)}</code>
                 </pre> */}
-                <div dangerouslySetInnerHTML={{ __html: SerializeToHtml(blocks) }}></div>
-            </div>
+                    <div dangerouslySetInnerHTML={{ __html: SerializeToHtml(blocks) }}></div>
+                </div>
+            )}
         </div>
     )
 }
 
-export const getServerSideProps = async (context) => {
-    try {
-        const serviceAccount = require('@/src/firebase-admin.json')
-        if (!admin.apps.length) {
-            admin.initializeApp({
-                credential: admin.credential.cert(serviceAccount),
-            })
-        }
+// export const getServerSideProps = async (context) => {
+//     try {
+//         const serviceAccount = require('@/src/firebase-admin.json')
+//         if (!admin.apps.length) {
+//             admin.initializeApp({
+//                 credential: admin.credential.cert(serviceAccount),
+//             })
+//         }
 
-        /**
-         * Get the id from the URL /pages/[id]
-         */
-        const document = await admin.firestore().collection('beta').doc(context.params.id).get()
+//         /**
+//          * Get the id from the URL /pages/[id]
+//          */
+//         const document = await admin.firestore().collection('beta').doc(context.params.id).get()
 
-        if (!document.exists) {
-            return {
-                notFound: true,
-            }
-        } else {
-            return {
-                props: {
-                    id: context.params.id,
-                    blocks: document.data().blocks,
-                },
-            }
-        }
-    } catch (err) {
-        console.log(err)
-        return {
-            props: {
-                error: true,
-            },
-        }
-    }
-}
+//         if (!document.exists) {
+//             return {
+//                 notFound: true,
+//             }
+//         } else {
+//             return {
+//                 props: {
+//                     id: context.params.id,
+//                     blocks: document.data().blocks,
+//                 },
+//             }
+//         }
+//     } catch (err) {
+//         console.log(err)
+//         return {
+//             props: {
+//                 error: true,
+//             },
+//         }
+//     }
+// }
 
 export default Page
