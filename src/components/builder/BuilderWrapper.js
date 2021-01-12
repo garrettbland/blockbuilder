0d4e1ad7816eeb4cf6f0cae75a49ebb5d069565f
garrettbland <gmorganbland@gmail.com>
2021-01-12 17:11:24 -0600

    setup initial modal

--- src/components/builder/BuilderWrapper.js
+++ src/components/builder/BuilderWrapper.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { Container } from 'react-smooth-dnd'
-import { SWAP_BLOCKS } from '@/redux/constants'
+import { SWAP_BLOCKS, SET_CUSTOM_MODAL } from '@/redux/constants'
 import Builder from './Builder'
 import NewSection from './section/NewSection'
 import Modal from './Modal'
@@ -22,6 +22,21 @@ const BuilderWrapper = () => {
         })
     }
 
+    /**
+     * Putting here for beta build
+     * Sets initial modal
+     */
+    useEffect(() => {
+        dispatch({
+            type: SET_CUSTOM_MODAL,
+            payload: {
+                visible: true,
+                component: <BetaModal />,
+                maxWidth: 'max-w-3xl',
+            },
+        })
+    }, [])
+
     return (
         <div>
             <Modal />
@@ -37,6 +52,46 @@ const BuilderWrapper = () => {
                 <NewSection />
             </div>
             <ActionButton />
+        </div>
+    )
+}
+
+const BetaModal = () => {
+    const dispatch = useDispatch()
+
+    const handleClose = () => {
+        dispatch({
+            type: SET_CUSTOM_MODAL,
+            payload: {
+                visible: false,
+            },
+        })
+    }
+
+    return (
+        <div>
+            <div className="p-4">
+                <p className="text-3xl font-black pb-3">Welcome to Block Builder</p>
+                <p className="text-gray-800 leading-tight text-xl pb-5">
+                    Block builder is a visual website builder made to quickly design websites. This
+                    is still very much in development, so thank you for trying it out. ❤️
+                </p>
+                <p className="text-gray-800 leading-tight text-xl pb-5">
+                    Please click around and explore. It's developed in React and TailwindCSS incase
+                    you're curious.
+                </p>
+                <p className="text-gray-800 leading-tight text-xl pb-5">
+                    You can save your work by pressing the big blue save button in the corner to get
+                    a live preview. If you have any feedback and have a moment, drop me a note at{' '}
+                    <a className="text-blue-500 hover:underline" href="mailto:garrettbland@hey.com">
+                        garrettbland@hey.com
+                    </a>
+                </p>
+                <p className="text-gray-800 leading-tight text-xl pb-12">Enjoy! (Hopefully 😀)</p>
+                <button onClick={() => handleClose()} className="text-gray-700 hover:underline">
+                    Close and start building
+                </button>
+            </div>
         </div>
     )
 }
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,17 +1,7 @@
-import React, { useEffect } from 'react'
-// import { useRouter } from 'next/router'
+import React from 'react'
 import BuilderWrapper from '@/components/builder/BuilderWrapper'
 
 const Home = () => {
-    // return <div>Home page</div>
-    // const router = useRouter()
-
-    // useEffect(() => {
-    //     if (router?.query?.page_id !== undefined) {
-    //         console.log(`Page id exists...${router.query.page_id}`)
-    //     }
-    // }, [router.query.page_id])
-
     return <BuilderWrapper />
 }
 
